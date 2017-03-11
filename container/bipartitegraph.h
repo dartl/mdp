7916506ee2a2a4eda9f438f552fe5fe80c116fde
dartl <twisan@mail.ru>
2017-03-11 21:15:24 +0300

    Итератор вершин
    Добавлен итератор вершин

--- container/bipartitegraph.h
+++ container/bipartitegraph.h
@@ -1,7 +1,9 @@
 #ifndef BIPARTITEGRAPH_H
 #define BIPARTITEGRAPH_H
 #include <list>
+#include <iostream>
 
+/* Шаблон вершины графа */
 template <class Type> class Node{
 private:
     bool check; // определяет, каким "цветом" окрашена вершина
@@ -27,8 +29,15 @@ public:
     void setCheck(bool c){
         check = c;
     }
+
+    friend std::ostream& operator<<(std::ostream& out, Node<Type> dt)
+    {
+        out << dt.getData();
+        return out;
+    }
 };
 
+/* Шаблон ребра графа(пара вершин) */
 template <class Type> class PairNode{
 private:
     Node<Type> *first;  // первая вершина ребра
@@ -53,6 +62,7 @@ public:
     }
 };
 
+/* Шаблон Двудольного графа */
 template <class Type> class BipartiteGraph
 {
 private:
@@ -60,12 +70,74 @@ private:
     std::list<PairNode<Type>> pairs;    // список ребер графа
 public:
     BipartiteGraph() {}
-    void add(Type n){
+    void addVertix(Type n){
         vertixs.push_front(new Node<Type>(n));
     }
-    void add(Type n, bool c){
+    void addVertix(Type n, bool c){
         Node<Type> temp(n,c);
-        vertixs.push_front(temp);
+        vertixs.push_back(temp);
+    }
+    void popFrontVertix() {
+        vertixs.pop_front();
+    }
+    void popBackVertix() {
+        vertixs.pop_back();
+    }
+
+    /* Итератор для списка вершин */
+    class IteratorVertixs {
+        typename std::list<Node<Type>>::iterator current = NULL;
+    public:
+        IteratorVertixs(typename std::list<Node<Type>>::iterator current):current(current){}
+        IteratorVertixs& operator=(const IteratorVertixs& other)
+        {
+            if (this != &other)
+            {
+                current = other.current;
+            }
+            return *this;
+        }
+
+        IteratorVertixs& operator++()
+        {
+            current++;
+            return *this;
+        }
+
+        IteratorVertixs& operator--()
+        {
+            current--;
+            return *this;
+        }
+
+        bool operator==(const IteratorVertixs& other)
+        {
+            return this->current.operator ==(other.current);
+        }
+
+        bool operator!=(const IteratorVertixs& other)
+        {
+            return this->current.operator !=(other.current);
+        }
+
+        Node<Type>& operator*(){
+            return *current;
+        }
+
+        Node<Type>* operator->()
+        {
+            else return current;
+        }
+
+        bool isEmpty() {
+            return current == NULL;
+        }
+    };
+    IteratorVertixs beginVertixs() {
+        return IteratorVertixs(vertixs.begin());
+    }
+    IteratorVertixs endVertixs() {
+        return IteratorVertixs(vertixs.end());
     }
 };
 
