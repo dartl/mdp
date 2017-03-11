9a5f4508e61e366fa05aea7c350ea5ca1835c8e8
dartl <twisan@mail.ru>
2017-03-11 17:53:02 +0300

    Node and PairNode
    Добавлены конструкторы, геттеры и сеттеры, для классов Node и PairNode

--- container/bipartitegraph.cpp
+++ container/bipartitegraph.cpp
@@ -1,4 +1,64 @@
 #include "bipartitegraph.h"
 
+/* !!! BipartiteGraph !!! */
 template <class Type> BipartiteGraph<Type>::BipartiteGraph(){
 }
+/* !!! BipartiteGraph END !!! */
+
+/* !!! Node !!! */
+template <class Type> Node<Type>::Node() {
+
+}
+
+template <class Type> Node<Type>::Node(Type d) {
+    data = d;
+}
+
+template <class Type> Node<Type>::Node(Type d, bool c) {
+    data = d;
+    check = c;
+}
+
+template <class Type> Type Node<Type>::getData() {
+    return data;
+}
+
+template <class Type> void Node<Type>::setData(Type d) {
+    data = d;
+}
+
+template <class Type> bool Node<Type>::isCheck() {
+    return check;
+}
+
+template <class Type> void Node<Type>::setCheck(bool c){
+    check = c;
+}
+/* !!! Node END !!! */
+
+/* !!! PairNode !!! */
+template <class Type> PairNode<Type>::PairNode(){
+
+}
+
+template <class Type> PairNode<Type>::PairNode(Node<Type> f,Node<Type> s){
+    first = &f;
+    second = &s;
+}
+
+template <class Type> Node<Type> PairNode<Type>::getFisrt() {
+    return *first;
+}
+
+template <class Type> void PairNode<Type>::setFisrt(Node<Type> f) {
+    first = &f;
+}
+
+template <class Type> Node<Type> PairNode<Type>::getSecond() {
+    return *second;
+}
+
+template <class Type> void PairNode<Type>::setSecond(Node<Type> s) {
+    second = s;
+}
+/* !!! PairNode END !!! */
--- container/bipartitegraph.h
+++ container/bipartitegraph.h
@@ -5,22 +5,35 @@
 template <class Type> class Node{
 private:
     bool check; // определяет, каким "цветом" окрашена вершина
-    Type data;
+    Type data;  // хранимый объект
 public:
-    Node(Type t);
+    Node(); // Пустой конструктор
+    Node(Type d);   // Конструктор с объектом
+    Node(Type d, bool c);   // Конструктор с объектом и "цветом" вершины
     Type getData();
+    void setData(Type d);
+    bool isCheck();
+    void setCheck(bool c);
 };
 
 template <class Type> class PairNode{
 private:
-    Node<Type> *first, *second;
+    Node<Type> *first,  // первая вершина ребра
+            *second;    // вторая вершина ребра
+public:
+    PairNode();
+    PairNode(Node<Type> f,Node<Type> s);
+    Node<Type> getFisrt();
+    void setFisrt(Node<Type> f);
+    Node<Type> getSecond();
+    void setSecond(Node<Type> s);
 };
 
 template <class Type> class BipartiteGraph
 {
 private:
-    std::list<Node<Type>> vertixs;
-    std::list<PairNode<Type>> pairs;
+    std::list<Node<Type>> vertixs;  // список вершин графа
+    std::list<PairNode<Type>> pairs;    // список ребер графа
 public:
     BipartiteGraph();
 };
