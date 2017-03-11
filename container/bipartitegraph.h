81109ebd116d8b46367cf54eb7f82c80ce36b44f
dartl <twisan@mail.ru>
2017-03-11 18:44:32 +0300

    Исправлено определние шаблона
    Определение функции шаблона перенесено из cpp в h файл, чтобы можно было
    корректно использовать щаблон

--- container/bipartitegraph.h
+++ container/bipartitegraph.h
@@ -7,26 +7,50 @@ private:
     bool check; // определяет, каким "цветом" окрашена вершина
     Type data;  // хранимый объект
 public:
-    Node(); // Пустой конструктор
-    Node(Type d);   // Конструктор с объектом
-    Node(Type d, bool c);   // Конструктор с объектом и "цветом" вершины
-    Type getData();
-    void setData(Type d);
-    bool isCheck();
-    void setCheck(bool c);
+    Node() {}
+    Node(Type d){
+        data = d;
+    }
+    Node(Type d, bool c){
+        data = d;
+        check = c;
+    }
+    Type getData() {
+        return data;
+    }
+    void setData(Type d) {
+        data = d;
+    }
+    bool isCheck(){
+        return check;
+    }
+    void setCheck(bool c){
+        check = c;
+    }
 };
 
 template <class Type> class PairNode{
 private:
-    Node<Type> *first,  // первая вершина ребра
-            *second;    // вторая вершина ребра
+    Node<Type> *first;  // первая вершина ребра
+    Node<Type> *second; // вторая вершина ребра
 public:
-    PairNode();
-    PairNode(Node<Type> f,Node<Type> s);
-    Node<Type> getFisrt();
-    void setFisrt(Node<Type> f);
-    Node<Type> getSecond();
-    void setSecond(Node<Type> s);
+    PairNode() {}
+    PairNode(Node<Type> f,Node<Type> s) {
+        first = &f;
+        second = &s;
+    }
+    Node<Type> getFisrt(){
+        return *first;
+    }
+    void setFisrt(Node<Type> f){
+        first = &f;
+    }
+    Node<Type> getSecond(){
+        return *second;
+    }
+    void setSecond(Node<Type> s){
+        second = s;
+    }
 };
 
 template <class Type> class BipartiteGraph
@@ -35,7 +59,14 @@ private:
     std::list<Node<Type>> vertixs;  // список вершин графа
     std::list<PairNode<Type>> pairs;    // список ребер графа
 public:
-    BipartiteGraph();
+    BipartiteGraph() {}
+    void add(Type n){
+        vertixs.push_front(new Node<Type>(n));
+    }
+    void add(Type n, bool c){
+        Node<Type> temp(n,c);
+        vertixs.push_front(temp);
+    }
 };
 
 #endif // BIPARTITEGRAPH_H
