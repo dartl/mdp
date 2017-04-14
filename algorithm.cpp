16540fa9bb17ab2096b6c2d8939ea21e0c6e651b
Sergey1994 <polovtsev-sergey@yandex.ru>
2017-04-15 00:53:29 +0300

    delete comments

--- algorithm.cpp
+++ algorithm.cpp
@@ -45,41 +45,6 @@ void Algorithm::addLeftNodeGraph(QString title)
 
 void Algorithm::addRightPartGraph()
 {
-//    int j = 0;
-//    for (BipartiteGraph<int>::IteratorVertixs i = (*graph).beginVertixs(); i != (*graph).endVertixs(); i++, j++)
-//    {
-
-//        if (!(*i).isCheck())
-//            graph->removeVertixNode(j);
-//            // TODO: удалить все вершины с параметром false
-//    }
-//    for (BipartiteGraph<int>::IteratorVertixs i = (*graph).beginVertixs(); i != (*graph).endVertixs(); i++)
-//    {
-//        if ((*i).isCheck()) {
-//            int currMax = -1;
-//            int workerMax = -1;
-//            int indexId = this->jobs->getIndexById((*i).getData());
-//            for (int j = 0; j < this->workers->elementsCount(); j++)
-//            {
-//                if (this->jobs->getTitle(indexId) == this->workers->getSpeciality(j))
-//                {
-//                      if (this->workers->getLevelSpec(j) > currMax)
-//                      {
-//                          workerMax = j;
-//                          currMax = this->workers->getLevelSpec(j);
-//                      }
-//                }
-
-//            }
-//            graph->addVertix(this->workers->getId(workerMax), false);
-//            graph->addPair(&(graph->getVertixNode((*i).getData())), &(graph->getVertixNode(this->workers->getId(workerMax))));
-
-//        }
-
-////        graph->addPair(&(graph->getVertixNode((*i).getData())), &(graph->getVertixNode(this->workers->getId(workerMax))));
-//    }
-//    PrintVertixs();
-//    PrintPairs();
 
     graph->removeAllFalse();
     for (BipartiteGraph<int>::IteratorVertixs i = (*graph).beginVertixs(); i != (*graph).endVertixs(); i++)
